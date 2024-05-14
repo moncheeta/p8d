df0c4a8a3feb787cdd40854a64bef25eeb9761fc
Damian Myrda <monkey.damianek@gmail.com>
2024-05-13 21:17:34 -0500

    Reboot after the updates

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ apt update -y && apt upgrade -y
 apt install -y certbot
 certbot certonly --standalone -d prime8.dev,mail.prime8.dev --email moncheeta@prime8.dev --agree-tos --force-renewal
 
+# apply all updates
+reboot
