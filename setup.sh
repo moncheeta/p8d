0df1cc52bf32903d73cc0095bd9ddb9e538b1890
Damian Myrda <monkey.damianek@gmail.com>
2024-05-13 21:32:19 -0500

    Move ssl setup to a seperate file

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt update -y && apt upgrade -y
 
 # ssl
 apt install -y certbot
-certbot certonly --standalone -d prime8.dev,mail.prime8.dev --email moncheeta@prime8.dev --agree-tos --force-renewal
+./ssl.sh
 
 # apply all updates
 reboot
