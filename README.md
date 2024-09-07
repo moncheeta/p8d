b25abf4526b71b6412f622a1bd092a31eb0b11bb
Damian Myrda <monkey.damianek@gmail.com>
2024-09-07 17:19:42 -0500

    Make some things more clear

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This is my website located at prime8.dev
 
-## running
+# download
+
+Run `sh <(curl -s https://raw.githubusercontent.com/moncheeta/p8d/master/download.sh)` to download the necessary files.
+
+# setup
+
+Run `setup.sh` to properly setup all the services.
+
+# run
 
 Run `docker compose up -d` to start up all the services in the background. You can stop them by running `docker compose down`.
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "setting up services"
-docker compose up -d
+docker compose up mail -d
 docker compose exec -it mail setup email add damian@prime8.dev
 docker compose restart mail
 docker compose exec -it mail setup alias add postmaster@prime8.dev damian@prime8.dev
 docker compose exec -it mail setup config dkim
 docker exec -it mail cat /tmp/docker-mailserver/opendkim/keys/prime8.dev/mail.txt
-docker compose restart mail
+docker compose down
 echo "services are set up"
