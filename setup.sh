fb2f3d8688a5c9ad7ca35322c15700e588c2fcc5
Damian Myrda <monkey.damianek@gmail.com>
2024-09-07 16:18:05 -0500

    Show dkim

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ docker compose exec -it mail setup email add damian@prime8.dev
 docker compose restart mail
 docker compose exec -it mail setup alias add postmaster@prime8.dev damian@prime8.dev
 docker compose exec -it mail setup config dkim
+docker exec -it mail cat /tmp/docker-mailserver/opendkim/keys/prime8.dev/mail.txt
 docker compose restart mail
 echo "services are set up"
